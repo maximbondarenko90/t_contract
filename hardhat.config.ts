6ae5a65806be41d13741f39af50c45ee60ed054a
maximbondarenko90 <maxim.bondarenko90@gmail.com>
2025-05-03 03:22:16 +0100

    Add files via upload

---
+++ hardhat.config.ts
@@ -0,0 +1,32 @@
+import '@openzeppelin/hardhat-upgrades';
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.23",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+      allowUnlimitedContractSize: true,
+      blockGasLimit: 20000000,
+      accounts: {
+        mnemonic: "test test test test test test test test test test test junk",
+        accountsBalance: "1000000000000000000000000",
+      }
+    },
+  },
+  mocha: {
+    timeout: 100000
+  }
+};
+
+export default config;
